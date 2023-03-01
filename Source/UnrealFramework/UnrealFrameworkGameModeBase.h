0e7dd083e503e4fe0502fe87fc2efe8d998f847e
幻想Fly <393830887@qq.com>
2023-03-01 16:14:26 +0800

    - 首次提交 - 添加忽略

---
+++ Source/UnrealFramework/UnrealFrameworkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealFrameworkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALFRAMEWORK_API AUnrealFrameworkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
